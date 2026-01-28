345945fffa79eabceebd0ecd23e58ce6719ca9f7
Samiul Arafah Dhrubo <66543604+arafah-dhrubo@users.noreply.github.com>
2026-01-28 23:34:50 +0600

    Update api_constants.dart

--- app/lib/core/constants/api_constants.dart
+++ app/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   // For local testing, you can use the following IP address
   // static const String baseUrl = 'http://192.168.10.73:8000/api';
-   static const String baseUrl = 'http://localhost:8000:8000/api';
+   static const String baseUrl = 'http://localhost:8000/api';
   static const String vitals = '$baseUrl/vitals';
   static const String analytics = '$vitals/analytics';
 }
